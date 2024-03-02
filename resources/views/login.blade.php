d9754cf41b6fe4fca4a8a8d36ce45820387e7d0f
nanayaw0542 <itmannanayaw@gmail.com>
2024-03-02 06:57:20 +0000

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,7 +11,7 @@
     <meta
       name="viewport"
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-    <title>Login Basic - Pages | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
+    <title>SMS | Login</title>
     <meta name="description" content="" />
     <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon/favicon.ico') }}" />
     <!-- Fonts -->
